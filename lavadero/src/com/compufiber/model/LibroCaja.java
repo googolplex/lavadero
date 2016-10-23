b36c1b4b596858d0d7b5af6e45bc85c4a9b26382
googolplex <googolplex@lcompras.biz>
2016-10-23 16:14:00 -0300

    completo la caja

--- lavadero/src/com/compufiber/model/LibroCaja.java
+++ lavadero/src/com/compufiber/model/LibroCaja.java
@@ -4,13 +4,64 @@ import java.util.*;
 
 import javax.persistence.*;
 
+import org.openxava.annotations.*;
 import org.openxava.util.*;
 
 @Entity
 @Table(name="LV_LIBROCAJA")
 public class LibroCaja extends SuperClaseFeliz {
 
+	@DescriptionsList(descriptionProperties="nombre")
+	@ManyToOne(fetch=FetchType.LAZY,optional=false)	
+	@JoinColumn(name="EMPLEADOS_ID")
+	private Empleados empleado ;
+
+	@DescriptionsList(descriptionProperties="cliente.nombre")
+	@ManyToOne(fetch=FetchType.LAZY,optional=false)	
+	@JoinColumn(name="TRABAJOSCAB_ID")
+	private TrabajosCab trabajo ;
+
 	
+	private Double montocobrado = 0.0D;
+
+	
+	
+	public Empleados getEmpleado() {
+		return empleado;
+	}
+
+
+
+	public void setEmpleado(Empleados empleado) {
+		this.empleado = empleado;
+	}
+
+
+
+	public TrabajosCab getTrabajo() {
+		return trabajo;
+	}
+
+
+
+	public void setTrabajo(TrabajosCab trabajo) {
+		this.trabajo = trabajo;
+	}
+
+
+
+	public Double getMontocobrado() {
+		return montocobrado;
+	}
+
+
+
+	public void setMontocobrado(Double montocobrado) {
+		this.montocobrado = montocobrado;
+	}
+
+
+
 	@PreUpdate
 	private void ultimoPaso() {
 			Date mifechora = new java.util.Date() ;
